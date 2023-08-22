f923b322ba6419035052d660d337dd45ebc5c38a
kelvin <kelvinokoye413@gmail.com>
2023-08-22 11:42:03 +0100

    done

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-const mainIMage = document.querySelector('.main__container');
+const mainImage = document.querySelector('.main__container');
 const mainImgWrapper = document.querySelector('.main__img__wrapper');
-console.log(mainImgWrapper);
+//            IMPLEMENTING PARRALAX SCROLLING.
+let scrollBottom = 0; // Store previous scroll position
+const minImgWrapperWidth = 25; // Minimum width in vw
 
-window.addEventListener('scroll', function (e) {
+window.addEventListener('scroll', function () {
   const scrollTop = window.scrollY;
-  console.log(e);
-  console.log(scrollTop);
-  mainIMage.style.backgroundPositionX = scrollTop * 0.9 + 'px';
-  //   mainImgWrapper.style.backgroundPosition = scrollTop * 1 + 'px';
+  //   adding parallax scroll on the main background image.
+  mainImage.style.backgroundPositionX = scrollTop * 0.9 + 'px';
+
+  // Calculate the change in scroll position
+  const scrollChange = scrollTop - scrollBottom;
+
+  // Calculate new width for mainImgWrapper based on scroll direction
+  let newWidth = parseFloat(
+    mainImgWrapper.style.width || minImgWrapperWidth + 'vw'
+  );
+
+  // Apply changes to width based on scroll direction
+  if (scrollChange > 0 && newWidth > 0) {
+    newWidth -= scrollChange * 0.01; // Adjust the rate of change here
+  } else if (scrollChange < 0 && newWidth < 100) {
+    newWidth -= scrollChange * 0.01; // Adjust the rate of change here
+  }
+
+  // Apply the new width to mainImgWrapper
+  mainImgWrapper.style.width = newWidth + 'vw';
+
+  // Update scrollBottom for the next iteration
+  scrollBottom = scrollTop;
 });
